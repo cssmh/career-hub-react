213548ccb0790a5f90d11cf320d83d37313bd0b5
momen210 <touristmomen@gmail.com>
2024-01-09 21:35:00 +0600

    job categories fully added

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,19 +1,59 @@
-import userPhoto from '../../assets/images/user.png'
+import { useEffect, useState } from "react";
+import userPhoto from "../../assets/images/user.png";
+import JobCategory from "../JobCategory/JobCategory";
 const Home = () => {
-    return (
-        <div>
-            <section className='my-12'>
-                <div className="px-3 lg:px-0 flex-row md:flex justify-between items-center">
-                    <div>
-                        <h1 className="text-3xl md:text-5xl font-extrabold">One Step<br></br> Closer To Your<br></br> <span className='text-[#7E90FE]'>Dream Job</span></h1>
-                        <p className='my-3 text-[#757575]'>Explore thousands of job opportunities with all the<br></br> information you need. Its your future. Come find it. Manage all<br></br> your job application from start to finish.</p>
-                        <button className='btn bg-gradient-to-r from-[#7E90FE] to-[#9873FF] text-white'>Get Started</button>
-                    </div>
-                    <img className="lg:w-2/5" src={userPhoto} alt="" />
-                </div>
-            </section>
+  const [jobCategory, setJobCategory] = useState([]);
+
+  useEffect(() => {
+    fetch("categories.json")
+      .then((res) => res.json())
+      .then((data) => setJobCategory(data));
+  }, []);
+
+  return (
+    <div>
+      {/* One Step Closer To Your Dream Job section */}
+      <section className="my-12">
+        <div className="px-3 lg:px-0 flex-row md:flex justify-between items-center">
+          <div>
+            <h1 className="text-3xl md:text-5xl font-extrabold">
+              One Step<br></br> Closer To Your<br></br>{" "}
+              <span className="text-[#7E90FE]">Dream Job</span>
+            </h1>
+            <p className="my-3 text-[#757575]">
+              Explore thousands of job opportunities with all the<br></br>{" "}
+              information you need. Its your future. Come find it. Manage all
+              <br></br> your job application from start to finish.
+            </p>
+            <button className="btn bg-gradient-to-r from-[#7E90FE] to-[#9873FF] text-white">
+              Get Started
+            </button>
+          </div>
+          <img className="lg:w-2/5" src={userPhoto} alt="" />
         </div>
-    );
+      </section>
+      {/* One Step Closer To Your Dream Job section ENd */}
+      {/* Job Category List Section */}
+      <section>
+        <div className="text-center">
+          <h1 className="text-2xl font-extrabold">Job Category List</h1>
+          <p className="text-[#757575] my-4">
+            Explore thousands of job opportunities with all the information you
+            need. Its your future
+          </p>
+          <div className="flex justify-center my-8 gap-5">
+            {jobCategory.map((singleCategory) => (
+              <JobCategory
+                key={singleCategory.id}
+                getCategory={singleCategory}
+              ></JobCategory>
+            ))}
+          </div>
+        </div>
+      </section>
+      {/* Job Category List Section ENd*/}
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/Component/JobCategory/JobCategory.jsx
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+const JobCategory = ({getCategory}) => {    
+    const { category_name, logo, availability } = getCategory
+    return (
+        <div className='text-left p-9 bg-gray-100'>
+            <img className='mb-3' src={logo} alt="" />
+            <p className='font-extrabold'>{category_name}</p>
+            <p className='text-[#A3A3A3]'>{availability}</p>
+        </div>
+    );
+};
+
+JobCategory.propTypes = {
+    getCategory: PropTypes.object,
+}
+
+export default JobCategory;
