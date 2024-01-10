3a8e3f790e3bbc226177d57e3c73931b6c262ce2
momen210 <touristmomen@gmail.com>
2024-01-10 18:16:34 +0600

    blogs and statistics component added with features

--- src/NavComponent/Statistics/Statistics.jsx
+++ src/NavComponent/Statistics/Statistics.jsx
@@ -1,9 +1,21 @@
+import { Discuss } from "react-loader-spinner";
+
 const Statistics = () => {
-    return (
-        <div>
-            Statistics
-        </div>
-    );
+  return (
+    <div className="flex justify-center mt-4">
+      <p>Under maintenance</p>
+      <Discuss
+        visible={true}
+        height="80"
+        width="80"
+        ariaLabel="discuss-loading"
+        wrapperStyle={{}}
+        wrapperClass="discuss-wrapper"
+        color="#fff"
+        backgroundColor="#F4442E"
+      />
+    </div>
+  );
 };
 
 export default Statistics;
