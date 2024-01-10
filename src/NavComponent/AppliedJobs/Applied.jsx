d8acff5d1c15dac15e1c3be4d0aa26f345d02780
momen210 <touristmomen@gmail.com>
2024-01-10 14:06:37 +0600

    applied job separated using filter with local include loader data

--- src/NavComponent/AppliedJobs/Applied.jsx
+++ src/NavComponent/AppliedJobs/Applied.jsx
@@ -6,18 +6,27 @@ const Applied = () => {
 
     const [appliedJob, setAppliedJob] = useState([])
 
-    const loadDataJobs = useLoaderData()
-
+    const allJobs = useLoaderData()
     useEffect(()=> {
-        const localStorageJobIds = getStoredThem();
-        if(loadDataJobs.length > 0){
-            const totalJobApplied = loadDataJobs.filter(job => localStorageJobIds.includes(job.id))
+        const localStorageJob = getStoredThem();
+        if(allJobs.length > 0){
+            const totalJobApplied = allJobs.filter(job => localStorageJob.includes(job.id))
             setAppliedJob(totalJobApplied);
+
+            // 2 ways
+            // const jobAppliedIdsAre = []
+            // for(const id of localStorageJob){
+            //     const matching = loadDataJobs.find(jobs => jobs.id === id)
+            //     if(matching){
+            //         jobAppliedIdsAre.push(matching)
+            //     }
+            // }
+            // setAppliedJob(jobAppliedIdsAre);
         }
-    },[])
+    },[allJobs])
     return (
         <div>
-            applied {appliedJob.length}
+            <h2 className="text-center font-extrabold text-2xl my-12">Applied Jobs {appliedJob.length}</h2>
         </div>
     );
 };
